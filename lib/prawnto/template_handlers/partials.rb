4b598cbd5b70dfc441a5f654ea8ba49b00bcbdc2
Forrest Zeisler <forrest@getjobber.com>
2012-02-27 17:38:38 -0800

    Handle nested partials.

--- lib/prawnto/template_handlers/partials.rb
+++ lib/prawnto/template_handlers/partials.rb
@@ -3,11 +3,17 @@ module Prawnto
     module Partials
 
       # Can be called within a prawn template to render a partial below it.
-      # :partial_name - current has to be the entire path from the views folder. Doesn't recognize the folder of the current template
-      # :prawn_object - defaults to the pdf document, but can take a paramenter to render within a prawn object. This is good for
-      #                 items like tables, text_blocks, etc.
+      # :partial_name - Current has to be the entire path from the views folder. Doesn't recognize the folder of the
+      #                 current template.
+      # :prawn_object - The object to use for the pdf object in the partial.
+      #                 Defaults to the pdf document, but can take a paramenter to render within a prawn object. This
+      #                 is good for items like tables, text_blocks, etc.
       def partial!(partial_name, prawn_object = pdf)
-        prawn_object.instance_eval partial_source(partial_name)
+        @pdf_stack ||= []
+        @pdf_stack.push @pdf
+        @pdf = prawn_object
+        instance_eval partial_source(partial_name)
+        @pdf = @pdf_stack.pop
       end
 
     private
--- spec/units/template_handlers/partials_spec.rb
+++ spec/units/template_handlers/partials_spec.rb
@@ -2,6 +2,7 @@ require "spec_helper"
 require "prawnto/template_handlers/partials.rb"
 
 class PartialTester
+  attr_accessor :pdf
   include Prawnto::TemplateHandlers::Partials
 end
 describe "Prawnto::TemplateHandlers::Partials" do
@@ -28,6 +29,40 @@ describe "Prawnto::TemplateHandlers::Partials" do
   describe "#partial_source" do
   end
 
-  describe "render_partial" do
+  describe "#render_partial" do
+    let(:prawn_object) { mock("prawn_object") }
+    let(:nested_prawn_object) { mock("nested_prawn_object") }
+    let(:prawn_document) {mock("prawn_document") }
+    before {
+      subject.stubs(:partial_source).returns("pdf.prawn_method")
+      subject.pdf = prawn_document
+    }
+
+    it "should default to rendering off the prawn document" do
+      prawn_document.expects(:prawn_method)
+      subject.partial! "some/path"
+    end
+
+    it "should default to rendering off the prawn document" do
+      prawn_object.expects(:prawn_method)
+      subject.partial! "some/path", prawn_object
+    end
+
+    it "should return to the original pdf object upon completion" do
+      prawn_object.stubs(:prawn_method)
+      subject.partial! "some/path", prawn_object
+      subject.pdf.should == prawn_document
+    end
+
+    it "should handle nested partials" do
+      subject.stubs(:nested_prawn_object).returns(nested_prawn_object)
+      subject.stubs(:partial_source).with("partial1").returns("partial! 'partial2', nested_prawn_object")
+      subject.stubs(:partial_source).with("partial2").returns("pdf.nested_prawn_method")
+
+      nested_prawn_object.expects("nested_prawn_method")
+      subject.partial! "partial1", prawn_object
+      subject.pdf.should == prawn_document
+    end
+
   end
 end
