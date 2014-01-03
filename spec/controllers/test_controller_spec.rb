320aa1da1ccc04d6d6ed8f388f697c6efd85e20c
Nathan Youngman <git@nathany.com>
2014-01-03 16:39:55 -0700

    update spec to expect syntax

--- spec/controllers/test_controller_spec.rb
+++ spec/controllers/test_controller_spec.rb
@@ -20,9 +20,11 @@ describe TestController do
 
     it "should render items in a block passed to a helper" do
       get :yield_block_in_helper_test, :format => :pdf
+      expect(response).to be_success
+
       asset_binary = File.open(asset_path("yield_block_in_helper_test")).read.bytes.to_a
       body_binary = response.body.bytes.to_a
-      body_binary.should == asset_binary
+      expect(body_binary).to eq(asset_binary)
     end
   end
 
@@ -30,30 +32,31 @@ describe TestController do
   describe "dsl" do
     it "returns correct PDF" do
       get :dsl_render, :format => :pdf
-      response.should be_success
+      expect(response).to be_success
 
       asset_binary = File.open(asset_path("dsl_render")).read.bytes.to_a
       body_binary = response.body.bytes.to_a
-      body_binary.should == asset_binary
+      expect(body_binary).to eq(asset_binary)
     end
   end
 
   describe "partials" do
     it "renders partials" do
       get :partial_render, :format => :pdf
-      response.should be_success
+      expect(response).to be_success
 
       asset_binary = File.open(asset_path("partial_render")).read.bytes.to_a
       body_binary = response.body.bytes.to_a
-      body_binary.should == asset_binary
+      expect(body_binary).to eq(asset_binary)
     end
   end
 
   describe "complex headers" do
     it "should return a file with a specified filename" do
       get :filename_test, :format => :pdf
-      response.should be_success
-      response.header["Content-Disposition"].should == "attachment;filename=\"fancy_name.pdf\""
+      expect(response).to be_success
+
+      expect(response.header["Content-Disposition"]).to eq("attachment;filename=\"fancy_name.pdf\"")
     end
 
   end
