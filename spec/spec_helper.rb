b34073891964ff68b64f0f352e5627adbafcb2ae
Nathan Youngman <git@nathany.com>
2014-06-24 10:13:24 -0600

    depend on prawn 1.0
    also update Rails versions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require 'minitest/autorun'
 require 'rspec/rails'
 require "mocha/setup"
 
